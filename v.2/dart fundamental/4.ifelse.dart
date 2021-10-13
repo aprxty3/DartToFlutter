8f7e9f118d3a5ae6ac993b577382309636388d26
aprxrt3 <ajiprxty3@gmail.com>
2021-10-13 12:50:23 +0700

    no message

--- v.2/dart fundamental/4.ifelse.dart
+++ v.2/dart fundamental/4.ifelse.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // Kondisi IF
+  print('berapa hasil pertambahan 10 + 5 ?');
+
+  //jika antara hasil pada int dan hasil pada if sama, maka akan muncul output
+  //jika keduanya berbeda maka output tidak akan muncul
+  int hasil = 15;
+  print(hasil);
+  if (hasil ==
+      15) /* ubah angka 15 menjadi 10 jika ingin menampilkan output */ {
+    print('Jawabanmu bener');
+  }
+
+  //Kondisi IF ELSE
+  
+}
