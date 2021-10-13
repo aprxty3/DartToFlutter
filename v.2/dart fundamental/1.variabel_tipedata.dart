a7be490cb651aba2d44d2e835b604e2e198b2bae
aprxrt3 <ajiprxty3@gmail.com>
2021-10-13 10:24:29 +0700

    no message

--- v.2/dart fundamental/1.variabel_tipedata.dart
+++ v.2/dart fundamental/1.variabel_tipedata.dart
@@ -3,7 +3,17 @@ void main(List<String> args) {
   int age = 23;
   var agee = 23;
 
-//number : double
+  //number : double
   double age1 = 23.8;
+  var agee1 = 23.8;
+
+  //String
+  String name = 'Aji';
+  var name1 = 'Aji';
+
+  //Boolean
+  bool isAlive = true;
+
   print(age1);
+  print(name);
 }
