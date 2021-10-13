75dc439646c44640692f1767704ab0e29b640f00
aprxrt3 <ajiprxty3@gmail.com>
2021-10-13 10:45:07 +0700

    no message

---
+++ v.2/dart fundamental/2.literals_stringliteral_stringinterpolation.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //literals = semua nilai yg bisa dimasukan pasa variabel
+  int age = 23;
+  double ageOnMonth = 8.5;
+  String name = 'Aji';
+  bool isAlive = true;
+
+  //string literals = variabel yg memiliki value, yg tipe datanya string atau text 
+
+
+  //string interpolation
+}
