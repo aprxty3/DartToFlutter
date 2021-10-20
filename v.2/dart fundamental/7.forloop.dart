72fd92194a8bd7f18854fae77ea7ac6c72c78dca
aprxrt3 <ajiprxty3@gmail.com>
2021-10-20 13:51:35 +0700

    for loop

--- v.2/dart fundamental/7.forloop.dart
+++ v.2/dart fundamental/7.forloop.dart
@@ -1,1 +1,13 @@
-void main(List<String> args) {}
+//for
+void main(List<String> args) {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+
+  //for in
+  List<String> angka = ['satu', 'dua', 'tiga'];
+
+  for (var item in angka) {
+    print(item);
+  }
+}
