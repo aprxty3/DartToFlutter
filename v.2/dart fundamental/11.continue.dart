1ebc66fbc7e3759a300f39a2a1789b2f8f229220
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 10:19:54 +0700

    no message

--- v.2/dart fundamental/11.continue.dart
+++ v.2/dart fundamental/11.continue.dart
@@ -6,10 +6,20 @@ void main(List<String> args) {
   //   print(i);
   // }
 
+  // for (var i = 1; i <= 3; i++) {
+  //   for (var j = 1; j <= 3; j++) {
+  //     if (i == 2 && j == 2) {
+  //       continue;
+  //     }
+  //     print('$i $j');
+  //   }
+  // }
+
+  outerLoop:
   for (var i = 1; i <= 3; i++) {
     for (var j = 1; j <= 3; j++) {
       if (i == 2 && j == 2) {
-        continue;
+        continue outerLoop;
       }
       print('$i $j');
     }
