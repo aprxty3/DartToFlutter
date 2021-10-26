d5fa4d4034e130d56765609b5605a68c04b312f6
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 14:05:46 +0700

    no message

--- v.2/dart fundamental/16.defaultparameter.dart
+++ v.2/dart fundamental/16.defaultparameter.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  volume(4, 2);
+}
+
+void volume(int panjang, int lebar, {int tinggi: 4}) {
+  print('hasil volumenya adalah ${panjang * lebar * tinggi}');
+}
