75cc6adae94b543fa831aa13739e44c175aabbee
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 14:40:36 +0700

    no message

--- v.2/dart intermediate/2.CustomExceptionClass.dart
+++ v.2/dart intermediate/2.CustomExceptionClass.dart
@@ -1,1 +1,19 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  try {
+    depositeUang(-10000);
+  } catch (error) {
+    print(error.errorMessage());
+  }
+}
+
+class DepositeException implements Exception {
+  String errorMessage() {
+    return 'kamu tidak bisa memasukan jumlah deposite dibawah 0';
+  }
+}
+
+void depositeUang(int jumlahUang) {
+  if (jumlahUang == 0) {
+    throw new DepositeException();
+  }
+}
