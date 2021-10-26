5c19cd076b744baafd4325e07bbaae7198e812fb
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 13:57:54 +0700

    no message

--- v.2/dart fundamental/15.namedparameter.dart
+++ v.2/dart fundamental/15.namedparameter.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  volume(4, 2, tinggi: 6);
+}
+
+void volume(int panjang, int lebar, {int tinggi}) {
+  print('hasil volumenya adalah ${panjang * lebar * tinggi}');
+}
