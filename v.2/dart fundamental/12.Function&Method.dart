ccf21e28e67f26e7fc204f40af5376a1202fb146
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 11:11:52 +0700

    no message

--- v.2/dart fundamental/12.Function&Method.dart
+++ v.2/dart fundamental/12.Function&Method.dart
@@ -1,6 +1,8 @@
 void main(List<String> args) {
   helloworld();
   luaspersegipanjang(4, 2);
+  int keliling = kelilingpersegipanjang(3, 5);
+  print("kelilingnya adalah $keliling");
 }
 
 void helloworld() {
@@ -10,4 +12,9 @@ void helloworld() {
 void luaspersegipanjang(int panjang, int lebar) {
   int luas = panjang * lebar;
   print("Luasnya adalah $luas");
+}
+
+int kelilingpersegipanjang(int panjang, int lebar) {
+  int keliling = 2 * (panjang + lebar);
+  return keliling;
 }
