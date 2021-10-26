62da34a3bb66be824b2d688ea380904cce13aa92
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 11:39:07 +0700

    no message

--- v.2/dart fundamental/14.positionalparameter.dart
+++ v.2/dart fundamental/14.positionalparameter.dart
@@ -1,1 +1,18 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // cuskota("Jakarta", 'Bandung', 'Semarang');
+  altcuskota("Jakarta", 'Bandung');
+}
+
+//Require Parameter
+// void cuskota(String kota1, String kota2, String kota3) {
+//   print('kota 1 adalah $kota1');
+//   print('kota 2 adalah $kota2');
+//   print('kota 3 adalah $kota3');
+// }
+
+//Positional Parameter
+void altcuskota(String kota1, String kota2, [String kota3]) {
+  print('kota 1 adalah $kota1');
+  print('kota 2 adalah $kota2');
+  print('kota 3 adalah $kota3');
+}
