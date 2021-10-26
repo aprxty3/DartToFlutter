ccdabad5fd2a8418fd740904796ea12e18f2c588
aprxrt3 <ajiprxty3@gmail.com>
2021-10-26 10:08:13 +0700

    no message

--- v.2/dart fundamental/10.break.dart
+++ v.2/dart fundamental/10.break.dart
@@ -1,8 +1,30 @@
 void main(List<String> args) {
-  for (var i = 1; i <= 10; i++) {
-    print(i);
-    if (i == 5) {
-      break;
+  // for (var i = 1; i <= 10; i++) {
+  //   print(i);
+  //   if (i == 5) {
+  //     break;
+  //   }
+  // }
+
+  // for (int i = 1; i <= 3; i++) {
+  //   for (int j = 1; j <= 3; j++) {
+  //     print("$i $j");
+
+  //     if (i == 2 && j == 2) {
+  //       break;
+  //     }
+  //   }
+  // }
+
+  outerLoop:
+  for (int i = 1; i <= 3; i++) {
+    innerLoop:
+    for (int j = 1; j <= 3; j++) {
+      print("$i $j");
+
+      if (i == 2 && j == 2) {
+        break outerLoop;
+      }
     }
   }
 }
