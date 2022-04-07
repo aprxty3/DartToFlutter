9e0422561924c02080531200689a5f966b3bc7c1
aprxty3 <ajiprxty3@gmail.com>
2022-04-07 23:40:25 +0700

    first

--- relearn/lib/main.dart
+++ relearn/lib/main.dart
@@ -7,13 +7,41 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List data = [
+    'data1',
+    'data2',
+    'data3',
+    'data4',
+    'data5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Dropdown',
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: DropdownButton(
+            items: data
+                .map(
+                  (e) => DropdownMenuItem(
+                    child: Text(e.toString()),
+                    value: e.toString(),
+                  ),
+                )
+                .toList(),
+            onChanged: (valeu) {},
           ),
         ),
       ),
