992aa5858884b84ea216241a6804f7556c78ad47
aprxty3 <ajiprxty3@gmail.com>
2022-04-08 23:01:43 +0700

    create main

--- relearn/lib/main.dart
+++ relearn/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'http_request/http_request.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,57 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  final List data = [
-    {
-      'judul': 'Pilihan ke - 1',
-      'data': '1',
-    },
-    {
-      'judul': 'Pilihan ke - 2',
-      'data': '2',
-    },
-    {
-      'judul': 'Pilihan ke - 3',
-      'data': '3',
-    },
-    {
-      'judul': 'Pilihan ke - 3',
-      'data': '3',
-    },
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Dropdown',
-        ),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: DropdownButton(
-            items: data
-                .map(
-                  (e) => DropdownMenuItem<String>(
-                    child: Text('${e['judul']}'),
-                    value: e['data'],
-                  ),
-                )
-                .toList(),
-            onChanged: (value) {
-              print(value);
-            },
-          ),
-        ),
-      ),
+      home: Homepage(),
     );
   }
 }
