c3c9e096a7b4167dc5147790c7cd50de76f0dc73
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 00:48:52 +0700

    consume API GET

--- relearn/lib/1.http_request/http_delete.dart
+++ relearn/lib/1.http_request/http_delete.dart
@@ -8,11 +8,47 @@ class http_Delete extends StatefulWidget {
 }
 
 class _http_DeleteState extends State<http_Delete> {
+  String data = 'Belum Ada Data';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('HTTP DELETE'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              var response =
+                  await http.get(Uri.parse('https://reqres.in/api/users/2'));
+
+              Map<String, dynamic> myBody = json.decode(response.body);
+
+              setState(() {
+                data =
+                    'NAMA : ${myBody['data']['first_name']} ${myBody['data']['last_name']}';
+              });
+            },
+            icon: Icon(
+              Icons.get_app,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          Text(
+            data,
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('DELETE'),
+          ),
+        ],
       ),
     );
   }
