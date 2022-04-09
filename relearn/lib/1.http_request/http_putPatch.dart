8f84a1a33e251c8f393aeb28aa1c49e1b8545201
aprxty3 <ajiprxty3@gmail.com>
2022-04-09 16:40:49 +0700

    build page

---
+++ relearn/lib/1.http_request/http_putPatch.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class http_PutPatch extends StatefulWidget {
+  @override
+  State<http_PutPatch> createState() => _http_PutPatchState();
+}
+
+class _http_PutPatchState extends State<http_PutPatch> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HTTP PUT/PATCH'),
+      ),
+    );
+  }
+}
