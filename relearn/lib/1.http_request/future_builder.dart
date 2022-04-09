6a341bcf00af645f98dd1705b77697dc184fd648
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 02:59:20 +0700

    try implement future builder hardcode

--- relearn/lib/1.http_request/future_builder.dart
+++ relearn/lib/1.http_request/future_builder.dart
@@ -8,33 +8,57 @@ class futureBuilder extends StatefulWidget {
 }
 
 class _futureBuilderState extends State<futureBuilder> {
+  Future getAllUser() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('FUTURE BUILDER'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            var response =
-                await http.get(Uri.parse('https://reqres.in/api/users'));
-            List data =
-                (json.decode(response.body) as Map<String, dynamic>)['data'];
-            print(data);
-            print('-------------------------');
-            print(data[0]);
-            print('-------------------------');
-            data.forEach((element) {
-              Map<String, dynamic> user = element;
-              print(user['email']);
-              print(user);
-              print('-------------------------');
-            });
-          },
-          child: Text('CLICK'),
+        appBar: AppBar(
+          title: Text('FUTURE BUILDER'),
         ),
-      ),
-    );
+        body: FutureBuilder(
+            future: getAllUser(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: Text('LOADING......'),
+                );
+              }
+              return ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(),
+                  title: Text('Nama Lengkap'),
+                  subtitle: Text('@gmail'),
+                ),
+              );
+            })
+
+        // Center(
+        //   child: ElevatedButton(
+        //     onPressed: () async {
+        //       var response =
+        //           await http.get(Uri.parse('https://reqres.in/api/users'));
+        //       List data =
+        //           (json.decode(response.body) as Map<String, dynamic>)['data'];
+        //       print(data);
+        //       print('-------------------------');
+        //       print(data[0]);
+        //       print('-------------------------');
+        //       data.forEach((element) {
+        //         Map<String, dynamic> user = element;
+        //         print(user['email']);
+        //         print(user);
+        //         print('-------------------------');
+        //       });
+        //     },
+        //     child: Text('CLICK'),
+        //   ),
+        // ),
+        );
   }
 }
