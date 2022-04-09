ff76a8d0af9b086875b098b7c1c939b9ebb6a2d6
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 02:35:01 +0700

    change route

--- relearn/lib/main.dart
+++ relearn/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:relearn/1.http_request/future_builder.dart';
 import 'package:relearn/1.http_request/http_delete.dart';
 import 'package:relearn/1.http_request/http_post.dart';
 import 'package:relearn/1.http_request/http_putPatch.dart';
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: http_Delete(),
+      home: futureBuilder(),
     );
   }
 }
