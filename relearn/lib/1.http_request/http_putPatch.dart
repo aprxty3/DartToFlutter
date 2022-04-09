2eb2b7e3cc329c16c721a74f2d11d75ac5694784
aprxty3 <ajiprxty3@gmail.com>
2022-04-09 23:01:42 +0700

    implement put

--- relearn/lib/1.http_request/http_putPatch.dart
+++ relearn/lib/1.http_request/http_putPatch.dart
@@ -8,16 +8,80 @@ class http_PutPatch extends StatefulWidget {
 }
 
 class _http_PutPatchState extends State<http_PutPatch> {
+  TextEditingController nameC = TextEditingController();
+  TextEditingController jobC = TextEditingController();
+
+  String hasilResponse = "BELUM ADA DATA";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HTTP PUT/PATCH'),
+        title: Text(
+          'Http PUT/PATCH',
+        ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: nameC,
+                autocorrect: false,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: jobC,
+                autocorrect: false,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Job',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  var myResponse = await http.put(
+                    Uri.parse('https://reqres.in/api/users/2'),
+                    body: {
+                      "name": nameC.text,
+                      "job": jobC.text,
+                    },
+                  );
+
+                  Map<String, dynamic> data =
+                      json.decode(myResponse.body) as Map<String, dynamic>;
+
+                  setState(() {
+                    hasilResponse = "${data['name']} - ${data['job']}";
+                  });
+                },
+                child: Text('CLICK'),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Divider(
+                thickness: 2,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(hasilResponse),
+            ],
+          ),
         ),
       ),
     );
