80956dd924aaa28b2856fe2db83b2958e6bc2373
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 22:40:57 +0700

    implement consume API

--- relearn/lib/http_request/latihan/latihan.dart
+++ relearn/lib/http_request/latihan/latihan.dart
@@ -1,16 +1,96 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:relearn/http_request/latihan/model/summary_model.dart';
 
-class LatihanHTTP extends StatefulWidget {
-  @override
-  State<LatihanHTTP> createState() => _LatihanHTTPState();
-}
+class LatihanHTTP extends StatelessWidget {
+  late Summary dataSummary;
 
-class _LatihanHTTPState extends State<LatihanHTTP> {
+  Future getSummary() async {
+    var response = await http.get(
+      Uri.parse('https://covid19.mathdro.id/api'),
+    );
+    Map<String, dynamic> data =
+        json.decode(response.body) as Map<String, dynamic>;
+    dataSummary = Summary.fromJson(data);
+
+    print(dataSummary.confirmed.value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('LATIHAN HTTP'),
+      ),
+      body: FutureBuilder(
+          future: getSummary(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Text('LOADING DATA....'),
+              );
+            }
+            return Column(
+              children: [
+                SummaryItems(
+                  "CONFIRMED",
+                  "${dataSummary.confirmed.value}",
+                ),
+                SummaryItems(
+                  "DEATHS",
+                  "${dataSummary.deaths.value}",
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
+
+class SummaryItems extends StatelessWidget {
+  String title;
+  String value;
+
+  SummaryItems(
+    this.title,
+    this.value,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(20),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 7,
+      child: Container(
+        height: 150,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.lightBlueAccent,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                value,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
