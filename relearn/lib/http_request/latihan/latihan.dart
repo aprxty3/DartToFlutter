dd33e757074d0ea63f6af8be63b01c2633e85d72
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 14:56:57 +0700

    build file

---
+++ relearn/lib/http_request/latihan/latihan.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LatihanHTTP extends StatefulWidget {
+  @override
+  State<LatihanHTTP> createState() => _LatihanHTTPState();
+}
+
+class _LatihanHTTPState extends State<LatihanHTTP> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LATIHAN HTTP'),
+      ),
+    );
+  }
+}
