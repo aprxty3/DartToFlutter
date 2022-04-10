b9b133874137c56363d3a7ae675909f2a8967987
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 14:54:55 +0700

    try using quicktype

--- relearn/lib/http_request/models/model_user.dart
+++ relearn/lib/http_request/models/model_user.dart
@@ -1,11 +1,31 @@
 class UserModel {
-  late String avatar;
-  late String name;
-  late String email;
-
   UserModel({
-    required this.avatar,
+    required this.id,
     required this.email,
-    required this.name,
+    required this.firstName,
+    required this.lastName,
+    required this.avatar,
   });
+
+  int id;
+  String email;
+  String firstName;
+  String lastName;
+  String avatar;
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
 }
