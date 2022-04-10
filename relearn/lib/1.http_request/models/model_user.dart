f09a1acf9d7ea52ddeee86145efc8a9b0f65963c
aprxty3 <ajiprxty3@gmail.com>
2022-04-10 14:35:12 +0700

    model

---
+++ relearn/lib/1.http_request/models/model_user.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  late String avatar;
+  late String name;
+  late String email;
+
+  UserModel({
+    required this.avatar,
+    required this.email,
+    required this.name,
+  });
+}
