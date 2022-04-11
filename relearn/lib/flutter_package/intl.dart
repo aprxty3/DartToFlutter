8459bd02c9849a4f8f2480e52402da8676e55a3b
aprxty3 <ajiprxty3@gmail.com>
2022-04-11 23:22:57 +0700

    build page

---
+++ relearn/lib/flutter_package/intl.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class INTL_Example extends StatelessWidget {
+  const INTL_Example({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('INTL EXAMPLE'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
+    );
+  }
+}
