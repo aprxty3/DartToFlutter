ed109d7ef454172b560e11f0e541ce48c418c792
aji prasetyo <ajiprasetyo@digitalsekuriti.id>
2022-04-20 02:47:32 +0700

    test google fonts

--- relearn/lib/flutter_package/avatar_glow.dart
+++ relearn/lib/flutter_package/avatar_glow.dart
@@ -1,5 +1,6 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Avatar_Glow extends StatelessWidget {
   @override
@@ -9,19 +10,31 @@ class Avatar_Glow extends StatelessWidget {
         title: Text('Avatar Glow'),
       ),
       body: Center(
-        child: AvatarGlow(
-          endRadius: 150.0,
-          glowColor: Colors.red,
-          child: Material(
-            // Replace this child with your own
-            elevation: 8.0,
-            shape: CircleBorder(),
-            child: CircleAvatar(
-              backgroundColor: Colors.grey[100],
-              backgroundImage: NetworkImage('https://picsum.photos/200'),
-              radius: 70.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AvatarGlow(
+              endRadius: 150.0,
+              glowColor: Colors.red,
+              child: Material(
+                // Replace this child with your own
+                elevation: 8.0,
+                shape: CircleBorder(),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[100],
+                  backgroundImage: NetworkImage('https://picsum.photos/200'),
+                  radius: 70.0,
+                ),
+              ),
             ),
-          ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'Ini namanya Avatar Glow',
+              style: GoogleFonts.nunitoSans(fontSize: 14),
+            )
+          ],
         ),
       ),
     );
